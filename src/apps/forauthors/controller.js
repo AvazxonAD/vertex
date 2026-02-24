37b16d15295d9b56b3ca8ba6d3c5c859ae26e44c
AvazxonAD <asqarovavazxon77@gmail.com>
2026-02-24 23:23:08 +0500

    vertex

--- src/apps/forauthors/controller.js
+++ src/apps/forauthors/controller.js
@@ -35,7 +35,7 @@ class Controller {
   static async getFile(req, res) {
     const { filename } = req.params;
 
-    const imagePath = path.join(process.cwd(), "public", "uploads", filename);
+    const imagePath = path.join(process.cwd(), process.env.UPLOAD_PATH, filename);
 
     if (!fs.existsSync(imagePath)) {
       return res.error(req.t("file_not_found"), 404);
